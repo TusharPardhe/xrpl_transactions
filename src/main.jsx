00e8429b871af2419d79c386493ec9696d211045
Tushar Pardhe <tushar.pardhe@airtel.com>
2022-09-03 02:51:18 +0530

    Vite polyfills added

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from './App'
 import './index.css';
 
-const Main = () => (
-  <Routes>
-    <Route path="/" element={<App />} />
-  </Routes>
-);
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Main />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 )
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import nodePolyfills from "rollup-plugin-polyfill-node";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
 
@@ -20,5 +21,22 @@ export default defineConfig({
         NodeModulesPolyfillPlugin(),
       ],
     },
-  }
+  },
+  rollupOptions: {
+    output: { entryFileNames: "[name].js" },
+    plugins: [nodePolyfills()],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+    alias: [
+      { find: "events", replacement: "rollup-plugin-node-polyfills/polyfills/events" },
+      { find: "child_process", replacement: "rollup-plugin-node-polyfills" },
+      { find: "path", replacement: "rollup-plugin-node-polyfills/polyfills/path" },
+    ],
+  },
+  esbuild: {
+    loader: "jsx",
+    include: /src\/.*\.jsx?$/,
+    exclude: [],
+  },
 })
